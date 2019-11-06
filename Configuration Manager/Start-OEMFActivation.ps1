0b2f9df982570502e2311bd5883b563de064037a
criostage <criostage@gmail.com>
2019-11-06 12:30:55 +0000

    Delete Start-OEMFActivation.ps1

--- Configuration Manager/Start-OEMFActivation.ps1
+++
@@ -1,5 +0,0 @@
-$ProductKey = (Get-WmiObject -Query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
-$service = Get-WmiObject -Query "select * from SoftwareLicensingService" -ComputerName $env:COMPUTERNAME
-$service.InstallProductKey($ProductKey)
-Start-Sleep 2
-$service.RefreshLicenseStatus()
