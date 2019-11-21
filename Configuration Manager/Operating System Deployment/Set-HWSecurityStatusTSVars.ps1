4c1e15501d9c99f8935ca959c246a62e3c14d29b
criostage <criostage@gmail.com>
2019-11-21 00:44:09 +0000

    Update Set-HWSecurityStatusTSVars.ps1

--- Configuration Manager/Operating System Deployment/Set-HWSecurityStatusTSVars.ps1
+++ Configuration Manager/Operating System Deployment/Set-HWSecurityStatusTSVars.ps1
@@ -1,4 +1,8 @@
-# Runs during WinPE Phase 
+<# 
+	Runs during WinPE Phase, before drive formatting.
+	Gathers and stores in Task Sequence Variables, information about the device TPM Chip readiness (if it's enabled, owned and active) and if
+	Secure Boot is enabled in UEFI. These Task Sequence Variables then can be used to trigger certain actions during the Task Sequence OSD.
+#>
 # Loading SCCM ComObject so we can set Task Sequence Variables
 $SMSTSEnvironment = New-Object -COMObject Microsoft.SMS.TSEnvironment
 
@@ -28,7 +32,7 @@ $SMSTSEnvironment.Value('TPMIsEnabled') = $TPMChipInfo.IsEnabled_InitialValue
 $SMSTSEnvironment.Value('BitLockerIsEnabled') = $ProtectionStatus -match "1"
 $SMSTSEnvironment.Value('SecureBootIsEnabled') = Confirm-SecureBootUEFI
 
-# Generate Random Bitlocker PIN
+# Generate Random Bitlocker PIN.
 if($TPMChipInfo.IsEnabled_InitialValue -eq $True){
 	$SMSTSEnvironment.Value('OSDBitlockerPIN') = ((1..$BitlockerPINlength) | ForEach-Object { Get-Random -Minimum 0 -Maximum 9 }) -join ''
 }
