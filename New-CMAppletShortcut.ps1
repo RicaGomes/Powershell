f62e352704975f5e334cc493418a54bb72557767
criostage <criostage@gmail.com>
2019-12-04 11:14:10 +0000

    Add files via upload
    To be used in testing machines

---
+++ New-CMAppletShortcut.ps1
@@ -0,0 +1,5 @@
+﻿$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Configuration Manager Applet.lnk")
+$Shortcut.TargetPath = "C:\Windows\CCM\SMSCFGRC.cpl"
+$Shortcut.IconLocation = "C:\Windows\CCM\SCClient.exe,0"
+$Shortcut.Save()
