3e81dcbe16c4eafcd70abee6130f8bf55c7eec35
criostage <criostage@gmail.com>
2019-12-14 00:14:21 +0000

    Add files via upload

---
+++ Configuration Manager/Server Setup/Create-SystemMgrADContainer.ps1
@@ -0,0 +1,27 @@
+Import-Module ActiveDirectory
+
+Function Create-SystemMgrADContainer{
+    Param(
+        [string]$ConfigMgrServer
+    )
+    $DomainInfo = Get-ADDomain
+    $ContainerName = "System Management"
+    $ContainerPath = "CN=System,$($DomainInfo.DistinguishedName)"
+    
+
+    if(!Test-Path -Path $ContainerPath){
+        $Container = New-ADObject -Type Container -name $ContainerName -Path $ContainerPath -PassThru    
+        $ContainerACL = (Get-ACL "AD:$($Container.DistinguishedName)")
+        $ConfigMgrServerSID = [System.Security.Principal.SecurityIdentifier](Get-ADComputer $ConfigMgrServer).SID
+    
+        $ADRights = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
+        $Type = [System.Security.AccessControl.AccessControlType] "Allow"
+        $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"  
+        $ACE = New-Object System.DirectoryServices.ActiveDirectoryAccessRule $ConfigMgrServerSID,$adRights,$type,$inheritanceType
+    
+        $ContainerACL.AddAccessRule($ACE)
+        Set-Acl -AclObject $ContainerACL "AD:$($Container.DistinguishedName)"
+    }
+}
+
+Create-SystemMgrADContainer -ConfigMgrServer $env:COMPUTERNAME
