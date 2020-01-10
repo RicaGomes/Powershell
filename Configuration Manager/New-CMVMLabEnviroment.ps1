be299af2bd2b3c6b1134aabe03e3267f8c9ff797
criostage <criostage@gmail.com>
2020-01-10 22:29:34 +0000

    Update New-CMVMLabEnviroment.ps1

--- Configuration Manager/New-CMVMLabEnviroment.ps1
+++ Configuration Manager/New-CMVMLabEnviroment.ps1
@@ -65,19 +65,33 @@ Function Create-VM{
     }
 }
 
-$Location = "G:\CMSetup"
-$ObjectPrefix = "Demo"
+
+$Location = "E:\DemoEnviroment"
+$SharedFolder = "$Location\Shared"
 $MediaFolder = "$Location\Media"
+$VMsFolder = "$Location\VMs"
+$ObjectPrefix = "Demo"
+
+if(-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){
+    Write-Warning "You are not running this as local administrator. Run it again in an elevated prompt." ; Exit 1
+}
+
+if(-not(Test-Path $Location)){
+    New-Item -Path $SharedFolder -ItemType Directory
+    New-SmbShare -Name "$ObjectPrefix$" -Path "$SharedFolder" -CachingMode None -FullAccess "Administrators" -Description "Shared folder to facilitate the large file transfers between Hyper-V and it's VMs" | Out-Null
+    New-Item -Path $MediaFolder -ItemType Directory
+    New-Item -Path $VMsFolder -ItemType Directory
+}
 
 # Gateway Machine (PFsense)
-Create-VM -VMName "$ObjectPrefix-GW1" -VMLocation $Location -VMSwitch NAT,"$ObjectPrefix-Datacenter","$ObjectPrefix-Tokyo" -VMCPUCores 2 -VMDiskSize 30 -VMMemory 768 -VMGen 2 -VMSecureBootOff -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "pfSense*" }).fullname
+Create-VM -VMName "$ObjectPrefix-GW1" -VMLocation $VMsFolder -VMSwitch InternalNATSwitch-1,"$ObjectPrefix-Datacenter","$ObjectPrefix-SiteA","$ObjectPrefix-SiteB" -VMCPUCores 2 -VMDiskSize 30 -VMMemory 1024 -VMGen 2 -VMSecureBootOff -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "pfSense*" }).fullname
 
 # Domain Controller (Windows Server Core 2019)
-Create-VM -VMName "$ObjectPrefix-DC1" -VMLocation $Location -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 2048 -VMGen 2 -VMTPM -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
+Create-VM -VMName "$ObjectPrefix-DC1" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMTPM -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
 
 # Configurarion Manager (Windows Server 2019)
-Create-VM -VMName "$ObjectPrefix-CM1" -VMLocation $Location -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 100,500,40,40,40 -VMMemory 12288 -VMGen 2 -VMTPM -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
+Create-VM -VMName "$ObjectPrefix-CM1" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 120,500,40,40,40 -VMMemory 12288 -VMGen 2 -VMTPM -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
 
 # Windows Enterprise Clients
-Create-VM -VMName "$ObjectPrefix-W10-1909-AD-01" -VMLocation $Location -VMSwitch "$ObjectPrefix-Tokyo" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 3072 -VMGen 2 -VMTPM
-Create-VM -VMName "$ObjectPrefix-W10-1909-AP-01" -VMLocation $Location -VMSwitch "$ObjectPrefix-Tokyo" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 3072 -VMGen 2 -VMTPM
+Create-VM -VMName "$ObjectPrefix-W10-1909-AD-01" -VMLocation $Location -VMSwitch "$ObjectPrefix-SiteA" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMTPM
+Create-VM -VMName "$ObjectPrefix-W10-1909-AP-01" -VMLocation $Location -VMSwitch "$ObjectPrefix-SiteA" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMTPM
