482fd1ad2e1ba6c39b43f2e5861e4053d15b64d4
criostage <criostage@gmail.com>
2020-02-07 19:53:02 +0000

    Update and rename New-CMVMLabEnviroment.ps1 to New-CMLabInfrastructure.ps1

--- Configuration Manager/New-CMLabInfrastructure.ps1
+++ Configuration Manager/New-CMLabInfrastructure.ps1
@@ -1,3 +1,25 @@
+Function Start-unGZip{
+    Param(
+        $infile,
+        $outfile = ($infile -replace '\.gz$','')
+    )
+
+    $input = New-Object System.IO.FileStream $inFile, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
+    $output = New-Object System.IO.FileStream $outFile, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+    $gzipStream = New-Object System.IO.Compression.GzipStream $input, ([IO.Compression.CompressionMode]::Decompress)
+
+    $buffer = New-Object byte[](1024)
+    while($true){
+        $read = $gzipstream.Read($buffer, 0, 1024)
+        if ($read -le 0){break}
+        $output.Write($buffer, 0, $read)
+    }
+
+    $gzipStream.Close()
+    $output.Close()
+    $input.Close()
+}
+
 Function Create-VMSwitch{
     Param(
         [Parameter (Mandatory =$true )]
@@ -65,33 +87,44 @@ Function Create-VM{
     }
 }
 
+$HyperVNatSwitchName = "NAT"
+$ObjectPrefix = "Navinet"
+$Location = "G:\Navinetwork"
 
-$Location = "E:\DemoEnviroment"
 $SharedFolder = "$Location\Shared"
 $MediaFolder = "$Location\Media"
 $VMsFolder = "$Location\VMs"
-$ObjectPrefix = "Demo"
+$pfsenseURL = "https://nyifiles.pfsense.org/mirror/downloads/pfSense-CE-2.4.4-RELEASE-p3-amd64.iso.gz"
+$pfsenseISO = ("$MediaFolder\$($pfsenseURL.Split("/")[-1])").Replace("\\","\")
+$WinSrv2019URL = "https://software-download.microsoft.com/download/pr/17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso"
+$WinSrv2019ISO =  ("$MediaFolder\$($WinSrv2019URL.Split("/")[-1])").Replace("\\","\")
 
 if(-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){
     Write-Warning "You are not running this as local administrator. Run it again in an elevated prompt." ; Exit 1
 }
 
 if(-not(Test-Path $Location)){
-    New-Item -Path $SharedFolder -ItemType Directory
+    $Output = New-Item -Path $SharedFolder -ItemType Directory
+    $Output = New-Item -Path $MediaFolder -ItemType Directory
+    $Output = New-Item -Path $VMsFolder -ItemType Directory
+
     New-SmbShare -Name "$ObjectPrefix$" -Path "$SharedFolder" -CachingMode None -FullAccess "Administrators" -Description "Shared folder to facilitate the large file transfers between Hyper-V and it's VMs" | Out-Null
-    New-Item -Path $MediaFolder -ItemType Directory
-    New-Item -Path $VMsFolder -ItemType Directory
+    Start-BitsTransfer -Source $pfsenseURL -Destination $pfsenseISO
+    Start-BitsTransfer -Source $WinSrv2019URL -Destination $WinSrv2019ISO
+    Start-unGZip -infile $pfsenseISO -outfile "$($pfsenseISO -replace '.gz','')"
+    Remove-Item -Path $pfsenseISO -force
 }
 
 # Gateway Machine (PFsense)
-Create-VM -VMName "$ObjectPrefix-GW1" -VMLocation $VMsFolder -VMSwitch InternalNATSwitch-1,"$ObjectPrefix-Datacenter","$ObjectPrefix-SiteA","$ObjectPrefix-SiteB" -VMCPUCores 2 -VMDiskSize 30 -VMMemory 1024 -VMGen 2 -VMSecureBootOff -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "pfSense*" }).fullname
+Create-VM -VMName "$ObjectPrefix-GW1" -VMLocation $VMsFolder -VMSwitch $HyperVNatSwitchName,"$ObjectPrefix-Datacenter","$ObjectPrefix-SiteA" -VMCPUCores 2 -VMDiskSize 30 -VMMemory 1024 -VMGen 2 -VMSecureBootOff -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "pfSense*" }).fullname
 
 # Domain Controller (Windows Server Core 2019)
-Create-VM -VMName "$ObjectPrefix-DC1" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMTPM -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
+Create-VM -VMName "$ObjectPrefix-DC1" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
 
 # Configurarion Manager (Windows Server 2019)
-Create-VM -VMName "$ObjectPrefix-CM1" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 120,200,40,40,40 -VMMemory 12288 -VMGen 2 -VMTPM -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
+Create-VM -VMName "$ObjectPrefix-CM1" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Datacenter" -VMCPUCores 4 -VMDiskSize 120,200,40,40,40 -VMMemory 16384 -VMGen 2 -VMISO (Get-ChildItem -Path $MediaFolder | Where-Object { $_.Name -like "*SERVER_EVAL*" }).fullname
 
 # Windows Enterprise Clients
-#Create-VM -VMName "$ObjectPrefix-W10-1909-AD-01" -VMLocation $Location -VMSwitch "$ObjectPrefix-SiteA" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMTPM
-#Create-VM -VMName "$ObjectPrefix-W10-1909-AP-01" -VMLocation $Location -VMSwitch "$ObjectPrefix-SiteA" -VMCPUCores 4 -VMDiskSize 50 -VMMemory 4096 -VMGen 2 -VMTPM
+#Create-VM -VMName "$ObjectPrefix-W10VM1909AD01" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Tokyo" -VMCPUCores 4 -VMDiskSize 60 -VMMemory 4096 -VMGen 2 -VMTPM
+#Create-VM -VMName "$ObjectPrefix-W10VM1909AD02" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Tokyo" -VMCPUCores 4 -VMDiskSize 60 -VMMemory 4096 -VMGen 2 -VMTPM
+#Create-VM -VMName "$ObjectPrefix-W10VM1909AD03" -VMLocation $VMsFolder -VMSwitch "$ObjectPrefix-Tokyo" -VMCPUCores 4 -VMDiskSize 60 -VMMemory 4096 -VMGen 2 -VMTPM
